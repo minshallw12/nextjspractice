d448bb953d1336ee69c2d4ca28dc55d9c4f79472
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-19 15:29:25 -0600

    ready for deployment

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hello! I am a computer enthusiast practicing Next.js for my next big project!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
